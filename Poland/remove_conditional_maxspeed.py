66acade946f2a642efac2d9b52bd7f7fb94b4921
Arkadiusz Maciaszczyk <arkadiusz.maciaszczyk@tomtom.com>
2021-12-09 12:19:13 +0100

    Update readme  Add license information

--- Poland/remove_conditional_maxspeed.py
+++ Poland/remove_conditional_maxspeed.py
@@ -1,3 +1,18 @@
+#    remove_conditional_maxspeed.py - Remove ceratin maxspeed tagging from OSM data in Poland
+#    Copyright (C) 2021 TomTom International B.V.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import logging
 import re
 from osm_bot_abstraction_layer.generic_bot_retagging import run_simple_retagging_task
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # osm-bots
+## Introduction
+This repository contains code of bots used to automatically update OSM data.
+In accordance to [Automated edits code of conduct](https://wiki.openstreetmap.org/wiki/Automated_Edits_code_of_conduct) each bot will have it's page describing the purpose and algorithm.
+Code is organized into directories descrbing areas the bot operates on.
+
+## Bot related pages
+### Poland
+- [remove_conditional_maxspeed.py](https://wiki.openstreetmap.org/wiki/Automated_edits/TTmechanicalupdates/Remove_night_time_conditional_speed_restriction_in_urban_areas_in_Poland)
