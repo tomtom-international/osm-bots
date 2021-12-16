b86af920f42c4310120e1752954c631cd3f2eba5
Bartosz Chrominski <bartosz.chrominski@tomtom.com>
2021-12-16 13:26:20 +0100

    Add changeset comment tag

--- bot-double-inner-ring/src/main/java/osm/bots/rings/inner/duplicates/verifiers/SourceVerifier.java
+++ bot-double-inner-ring/src/main/java/osm/bots/rings/inner/duplicates/verifiers/SourceVerifier.java
@@ -6,6 +6,7 @@ import osm.bots.rings.inner.duplicates.osmapi.model.ViolatingOsmData;
 
 import java.util.Collection;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 @Slf4j
 class SourceVerifier extends Verifier {
@@ -18,31 +19,28 @@ class SourceVerifier extends Verifier {
 
     @Override
     boolean isMatchingVerifierCriteria(ViolatingOsmData violatingOsmData) {
-        Optional<String> relationSourceTagValue = getSourceTagValue(violatingOsmData.getRelation());
-        Optional<String> duplicatedWaySourceTagValue = getSourceTagValue(violatingOsmData.getDuplicatingWay().getWay());
-        if (relationSourceTagValue.isEmpty()) {
-            return false;
-        }
-        if (relationSourceTagValue.equals(duplicatedWaySourceTagValue)) {
-            return isSourceValueAllowed(relationSourceTagValue.get(), violatingOsmData);
-        } else {
-            log.warn("Different Source tag value between duplicating way: {} and relation: {} for violation {}",
-                    relationSourceTagValue.get(), duplicatedWaySourceTagValue.orElse(""), violatingOsmData);
-            return false;
-        }
+        return hasValidSourceTag(violatingOsmData.getRelation(), violatingOsmData)
+                && hasValidSourceTag(violatingOsmData.getDuplicatingWay().getWay(), violatingOsmData);
     }
 
-    private boolean isSourceValueAllowed(String relationSourceTagValue, ViolatingOsmData violatingOsmData) {
-        if (configuredTagValues.contains(relationSourceTagValue)) {
-            return true;
-        } else {
-            log.warn("Source tag value is present but its value: {} is other than configured for violation {}", relationSourceTagValue, violatingOsmData);
-            return false;
-        }
+    private boolean hasValidSourceTag(Element element, ViolatingOsmData violatingOsmData) {
+        return getSourceTagValue(element)
+                .filter(isSourceValueAllowed(violatingOsmData))
+                .isPresent();
     }
 
     private Optional<String> getSourceTagValue(Element osmElement) {
         String sourceValue = osmElement.getTags().get("source");
         return Optional.ofNullable(sourceValue);
+    }
+
+    private Predicate<String> isSourceValueAllowed(ViolatingOsmData violatingOsmData) {
+        return sourceTagValue -> {
+            boolean sourceInConfig = configuredTagValues.contains(sourceTagValue);
+            if (!sourceInConfig) {
+                log.warn("Source tag value is present but its value: {} is other than configured for violation {}", sourceTagValue, violatingOsmData);
+            }
+            return sourceInConfig;
+        };
     }
 }
